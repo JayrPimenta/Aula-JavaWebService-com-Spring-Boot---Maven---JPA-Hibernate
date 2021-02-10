f882789eec411ec8139b3a76a6b47d4cff7ae298
Jayr Pimenta <jayr.pimenta@gmail.com>
2021-02-10 11:42:28 -0300

    Implementado o metodo buscarPorId()

--- src/main/java/com/javawebservice/curso/resources/UsuarioResource.java
+++ src/main/java/com/javawebservice/curso/resources/UsuarioResource.java
@@ -1,20 +1,36 @@
 package com.javawebservice.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javawebservice.curso.entidades.Usuario;
+import com.javawebservice.curso.services.UsuarioServices;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
 
+	@Autowired
+	private UsuarioServices service;
+	
 	@GetMapping
-	public ResponseEntity<Usuario> buscarTodos() {
-		Usuario user = new Usuario(1l, "Jayr", "jayr.pimenta@gmail.com", "998146022", "1234");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<Usuario>> buscarTodos() {
+		
+		List<Usuario> listaUsuarios = service.buscarTodos();
+		
+		return ResponseEntity.ok().body(listaUsuarios);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id){
+		Usuario usuario = service.buscarPorId(id);
+		return ResponseEntity.ok().body(usuario);
 	}
 	
 }
---
+++ src/main/java/com/javawebservice/curso/services/UsuarioServices.java
@@ -0,0 +1,29 @@
+package com.javawebservice.curso.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javawebservice.curso.entidades.Usuario;
+import com.javawebservice.curso.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioServices {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public List<Usuario> buscarTodos() {
+		return usuarioRepository.findAll();
+		
+	}
+	
+	public Usuario buscarPorId(Long id) {
+		
+//		Optional<Usuario> user = usuarioRepository.findById(id);
+//		return user.get();
+		
+		return usuarioRepository.findById(id).get();
+	}
+}
